f112dca00042b3127c4f39d0e6a878728489922a
Zander Mackie <zmackie@gmail.com>
2018-10-18 19:21:53 -0400

    Reading file and getting tty

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::fs;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
+use std::str;
+
+fn main() -> std::io::Result<()> {
+  let tty = fs::OpenOptions::new()
+    .read(true)
+    .write(true)
+    .open("/dev/tty")?;
+
+  let mut tty = BufReader::new(tty);
+  let mut piped = String::new();
+  io::stdin().read_to_string(&mut piped)?;
+
+  println!("piped:\n{}", piped);
+  let mut buf = Vec::<u8>::new();
+
+  loop {
+    let num_bytes = tty.read_until(b'\n', &mut buf)?;
+    if buf.starts_with(&[27]) {
+      println!("Escape!\n{:?}", buf);
+      buf.clear();
+      continue;
+    }
+    if num_bytes == 0 {
+      break;
+    }
+    buf.pop();
+    println!("{}", str::from_utf8(&buf).unwrap());
+    buf.clear();
+  }
+
+  Ok(())
+}
